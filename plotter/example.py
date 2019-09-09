e1d8d96f01718cd1c3cb53b8e8897fbabd427ff0
Mike Wilson <mail4mikew@gmail.com>
2019-09-09 15:30:56 -0700

    change example code used to make recent changes to live_data.

--- plotter/example.py
+++ plotter/example.py
@@ -1,19 +1,65 @@
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.uic import loadUi
+# -*- coding: utf-8 -*-
+"""
+Various methods of drawing scrolling plots.
+"""
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import numpy as np
 
-import sys
+win = pg.GraphicsWindow()
+win.setWindowTitle('pyqtgraph example: Scrolling Plots')
 
-class Window(QtWidgets.QMainWindow):
-    def __init__(self):
-        QtWidgets.QWidget.__init__(self)
-        loadUi("window.ui", self)
 
-        self.label.setFont(QtGui.QFont('SansSerif', 30)) # change font type and size
- 
+# 1) Simplest approach -- update data in the array such that plot appears to scroll
+#    In these examples, the array size is fixed.
+p1 = win.addPlot()
+data1 = np.random.normal(size=300)
+curve1 = p1.plot(data1)
+ptr1 = 0
+def update1():
+    global data1, curve1, ptr1
+    data1[:-1] = data1[1:]  # shift data in the array one sample left
+                            # (see also: np.roll)
+    data1[-1] = np.random.normal()
+    curve1.setData(data1)
+    ptr1 += 1
 
-if __name__ == "__main__":
+# 2) Allow data to accumulate. In these examples, the array doubles in length
+#    whenever it is full. 
+win.nextRow()
+p4 = win.addPlot()
+# Use automatic downsampling and clipping to reduce the drawing load
+p4.setDownsampling(mode='peak')
+p4.setClipToView(True)
+curve4 = p4.plot()
+
+data3 = np.empty(100)
+ptr3 = 0
+
+def update2():
+    global data3, ptr3
+    data3[ptr3] = np.random.normal()
+    ptr3 += 1
+    if ptr3 >= data3.shape[0]:
+        tmp = data3
+        data3 = np.empty(data3.shape[0] * 2)
+        data3[:tmp.shape[0]] = tmp
+    curve4.setData(data3[:ptr3])
+
+
+# update all plots
+def update():
+    update1()
+    update2()
+
+timer = pg.QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)
+
+
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
     import sys
-    app = QtWidgets.QApplication(sys.argv)
-    w = Window()
-    w.show()
-    sys.exit(app.exec_())
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
