3178ad3dc7231b7d742434ae57c60ae0cd527778
Mike Wilson <mail4mikew@gmail.com>
2019-09-09 15:30:14 -0700

    Fix variety of plotting issues
    - use correct parent object
    - separate the plots into two graphic objects
    - change names of some objects to make them clearer
    - lr becomes persistent so we can update with data changes
    - stop terminating the top plot, it follows the bottom one now
    - move the update function into the window object for easier access to internals

--- plotter/live_data.py
+++ plotter/live_data.py
@@ -11,44 +11,38 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
         # self.setupUi(self) # Set up the user interface from Designer.
         uic.loadUi('data_scope.ui', self)
-        #self.graph.nextRow()
-        
-        lr = pg.LinearRegionItem([400,700])
-        lr.setZValue(-10)
-        
-        self.p1 = self.graph.addPlot(title="Zoom on selected region")
-        
-        def updatePlot():
-            self.p1.setXRange(*lr.getRegion(), padding=0)
-        def updateRegion():
-            lr.setRegion(self.p1.getViewBox().viewRange()[0])
-        lr.sigRegionChanged.connect(updatePlot)
-        self.p1.sigXRangeChanged.connect(updateRegion)
 
+        self.lr = pg.LinearRegionItem([0, 100])
+        self.lr.setZValue(-10)
+
+        self.plots = self.graph.addPlot(title = "Captured data")
+        self.plots.setDownsampling(mode = 'peak')
+        self.plots.setClipToView(True)
+
+        def updatePlot():
+            self.plots.setXRange(*self.lr.getRegion(), padding = 0)
+            print("updatePlot()")
+
+        def updateRegion():
+            self.lr.setRegion(self.plots.getViewBox().viewRange()[0])
+            print("updateRegion()")
+
+        self.lr.sigRegionChanged.connect(updatePlot)
+        self.plots.sigXRangeChanged.connect(updateRegion)
         updatePlot()
 
-        self.graph.nextRow()
-        self.p2 = self.graph.addPlot(title="Region Selection")
-        self.p2.showAxis('bottom', False)
-        self.p2.addItem(lr)
-        # Allow data to accumulate. Array doubles in length whenever it is full. 
-        # Use automatic downsampling and clipping to reduce the drawing load
-        self.p1.setDownsampling(mode='peak')
-        self.p2.setDownsampling(mode='peak')
-        self.p1.setClipToView(True)
-        self.p2.setClipToView(True)
-        self.p1.setRange(xRange=[-100, 0])
-        self.p1.setLimits(xMax=0)
-        self.curve1 = self.p1.plot(pen=(255,255,255,200))
-        self.curve2 = self.p2.plot()
-class DataUpdate():
-    def __init__(self, curves,terms):
-        self.curves = curves
-        self.terms = terms
-        self.data = np.empty(100)  # @UndefinedVariable
+        self.scroll = self.graphScroll.addPlot(title = "Select focus")
+        self.scroll.showAxis('bottom', False)
+        self.scroll.addItem(self.lr)
 
+        self.scroll.setClipToView(True)
+        self.scroll.setDownsampling(mode = 'peak')
 
+        self.curve1 = self.plots.plot()
+        self.curve2 = self.scroll.plot()
 
+        self.terms = [True, False]
+        self.data = np.empty(100)
         self.ptr = 0
 
         self.timer = pg.QtCore.QTimer()
@@ -64,19 +58,18 @@ class DataUpdate():
             self.data = np.empty(self.data.shape[0] * 2)
             self.data[:tmp.shape[0]] = tmp
 
-        for curve, term in zip(self.curves, self.terms):    
+        for curve in [self.curve1, self.curve2]:
             curve.setData(self.data[:self.ptr])
-            if term:
-                curve.setPos(-self.ptr, 0)
 
+        print("update setRegion")
+        self.lr.setRegion([0, self.ptr])
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
 
     # timer based threads which produce the data updates
-    update1 = DataUpdate([window.curve1, window.curve2], [True,False])
-    update1.timer.start(20)
+    window.timer.start(20)
 
     window.show()
     app.exec_()
