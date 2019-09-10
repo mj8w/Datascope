09687c196139820b57baee368fc1db05c1c79181
Mike Wilson <mail4mikew@gmail.com>
2019-09-09 19:03:52 -0700

    remove dead code from decomp files.

--- plotter/decomp_thread.py
+++ plotter/decomp_thread.py
@@ -2,21 +2,10 @@
 """
 Decompress binary data into plottable points
 """
-from time import sleep
-from queue import Queue
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 from _struct import unpack
 
-#try:
-#    from config import configuration as cfg  # @UnusedImport
-#except:
-    
-from config_default import configuration as cfg  # @Reimport
-
 from serial import Serial, SerialException
-
-from plotter import logset  # @UnresolvedImport
-
+from plotter import logset # @UnresolvedImport
 debug, info, warn, err = logset('decomp')
 
 class DecompBinary():
@@ -25,56 +14,47 @@ class DecompBinary():
     """
 
     def __init__(self):
-        self.delim = cfg["pkg_delimiter"]
-        
-        # get the data size for each signal type
-        self.id_slots = [None for _ in range(cfg["MAX_SIGNAL"])]
-        for k in cfg:
-            if cfg[k].__class__.__name__ == "Signal":
-                self.id_slots[cfg[k].id] = cfg[k].precision
-        info("{!r}".format(self.id_slots) )
-        
         # initialize a co-routine used to simplify logic of building messages
         self.ipt = self.build_pt()
         next(self.ipt)
-        
+
     def open_serial(self, source):
         """ Open the source of data and set the queue to read from """
-        self.source = Serial(source, 115200, timeout = 0.05)  # timeout set for visual continuity on graph
+        self.source = Serial(source, 115200, timeout = 0.05) # timeout set for visual continuity on graph
         self.source.open()
-        
+
     def read_queue(self): # A slot which takes no params
         """ Read the data source, interpret the data into graphable points """
-        info("read_queue()") 
+        info("read_queue()")
         msg = []
         while(1):
             try:
                 abyte = self.source.read(1)
             except SerialException:
                 yield None
-               
+
             msg.append(abyte)
-                
-            # pass 'by' to the packager, and pkg      
+
+            # pass 'by' to the packager, and pkg
             pkg = self.ipt.send(abyte)
             if pkg != None:
                 # a package was decoded, so put it in the data stream
                 yield pkg
 
-                # record the activity to the logging system                
+                # record the activity to the logging system
                 timestamp , data_type, value = pkg
                 if len(msg) < 10:
                     info("{:32s}{:f}, {}, {:f}".format(
-                        " ".join(["{:02X}".format(b) for b in bytes(msg)]), 
-                        timestamp, 
-                        data_type, 
+                        " ".join(["{:02X}".format(b) for b in bytes(msg)]),
+                        timestamp,
+                        data_type,
                         value))
                 else:
-                    for i in range(0,len(msg),8):
-                        th8 = msg[i:min(i+8,len(msg))]
+                    for i in range(0, len(msg), 8):
+                        th8 = msg[i:min(i + 8, len(msg))]
                         info("{}".format(" ".join(["{:02X}".format(b) for b in th8])))
                 msg = []
-                 
+
     def build_pt(self):
         """ 
             Attempt to create a datapoint using the byte stream input 
@@ -85,10 +65,10 @@ class DecompBinary():
         while(1):
             while by != self.delim:
                 by = yield None
-                
+
             # have delim - next get timestamp
             timestamp = yield None
-            for _i in [1,2,3]:
+            for _i in [1, 2, 3]:
                 by = yield None
                 timestamp = (timestamp * 256) + by
             by = yield None
@@ -97,16 +77,16 @@ class DecompBinary():
                 if self.id_slots[data_type] == None:
                     continue # restart search for valid characters
             except IndexError:
-                continue    # restart search for valid characters
-            
+                continue # restart search for valid characters
+
             # get the conversion characteristics for the data field
             conv, mant, fract = self.id_slots[data_type]
             bys = []
-            for _ in range(int((mant+fract+1)/8)):
+            for _i in range(int((mant + fract + 1) / 8)):
                 by = yield None
                 bys.append(by)
-            
+
             value = unpack(conv, bytes(bys))[0]
             value /= 1 << fract
-            by = yield (timestamp / 1000000., data_type, value) #finally return a value
+            by = yield (timestamp / 1000000., data_type, value) # finally return a value
 
