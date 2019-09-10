e0135a84465045e25a277fb6d51295dd40756fec
Mike Wilson <mail4mikew@gmail.com>
2019-09-09 17:13:23 -0700

    Convert plots to acommodate up to 4 signals (4 colors)

--- plotter/live_data.py
+++ plotter/live_data.py
@@ -12,40 +12,54 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.setupUi(self) # Set up the user interface from Designer.
         uic.loadUi('data_scope.ui', self)
 
+        # Graph window
+
+        self.scope = self.graph.addPlot(title = "Captured data")
+        self.scope.setDownsampling(mode = 'peak')
+        self.scope.setClipToView(True)
+
+        # Scroll window
+
+        self.scroll = self.graphScroll.addPlot(title = "Select focus")
+        self.scroll.setClipToView(True)
+        self.scroll.setDownsampling(mode = 'peak')
+        self.scroll.showAxis('bottom', False)
+
+        self.plot_count = 4
+
+        self.plots = [
+            self.scope.plot(pen = (255, 0, 0), name = "Red"),
+            self.scope.plot(pen = (0, 255, 0), name = "Green"),
+            self.scope.plot(pen = (0, 0, 255), name = "Blue"),
+            self.scope.plot(pen = (0, 255, 255), name = "Aqua"),
+            ]
+
+        self.scroll_plots = [
+            self.scroll.plot(pen = (255, 0, 0), name = "Red"),
+            self.scroll.plot(pen = (0, 255, 0), name = "Green"),
+            self.scroll.plot(pen = (0, 0, 255), name = "Blue"),
+            self.scroll.plot(pen = (0, 255, 255), name = "Aqua"),
+            ]
+
+        # "Linear region" - selection in scroll window that controls viewing of main window
+
         self.lr = pg.LinearRegionItem([0, 100])
         self.lr.setZValue(-10)
-
-        self.plots = self.graph.addPlot(title = "Captured data")
-        self.plots.setDownsampling(mode = 'peak')
-        self.plots.setClipToView(True)
+        self.scroll.addItem(self.lr)
 
         def updatePlot():
-            self.plots.setXRange(*self.lr.getRegion(), padding = 0)
+            self.scope.setXRange(*self.lr.getRegion(), padding = 0)
             print("updatePlot()")
 
         def updateRegion():
-            self.lr.setRegion(self.plots.getViewBox().viewRange()[0])
+            self.lr.setRegion(self.scope.getViewBox().viewRange()[0])
             print("updateRegion()")
 
         self.lr.sigRegionChanged.connect(updatePlot)
-        self.plots.sigXRangeChanged.connect(updateRegion)
+        self.scope.sigXRangeChanged.connect(updateRegion)
         updatePlot()
 
-        self.scroll = self.graphScroll.addPlot(title = "Select focus")
-        self.scroll.showAxis('bottom', False)
-        self.scroll.addItem(self.lr)
-
-        self.scroll.setClipToView(True)
-        self.scroll.setDownsampling(mode = 'peak')
-
-        self.curve1 = self.plots.plot()
-        self.curve2 = self.scroll.plot()
-
-        self.terms = [True, False]
-        self.data = np.empty(100)
-        self.ptr = 0
-
-        # process Capture button feature
+        # Capture button features
 
         # the capture button
         self.capture_Button.setCheckable(True)
@@ -54,28 +68,35 @@ class MainWindow(QtWidgets.QMainWindow):
         self.timer = pg.QtCore.QTimer()
         self.timer.timeout.connect(self.update)
 
+        self.data = np.empty([self.plot_count, 100])
+        self.ptr = [0 for _i in range(self.plot_count)]
+        self.max_data = 0
+
     def onButtonToggle(self, checked):
         if(checked):
-            # timer based threads which produce the data updates
-            self.timer.start(20)
+            self.timer.start(20) # start the timer based thread which produces the data updates
+            self.timestamp = 0 # temporary
         else:
             self.timer.stop()
 
     def update(self):
-        self.data[self.ptr] = np.random.normal()
-        self.ptr += 1
+        channel = np.random.random_integers(0, 3)
+        i = self.ptr[channel]
+        self.data.itemset((channel, i), np.random.normal())
+        self.ptr[channel] += 1
 
         # re-shape data if it gets too big.
-        if self.ptr >= self.data.shape[0]:
-            tmp = self.data
-            self.data = np.empty(self.data.shape[0] * 2)
-            self.data[:tmp.shape[0]] = tmp
+        if self.ptr[channel] >= self.data.shape[1]:
+            np.reshape(self.data, [self.plot_count, self.data.shape[1] * 2])
 
-        for curve in [self.curve1, self.curve2]:
-            curve.setData(self.data[:self.ptr])
+        # apply the updated data to the curves
+        i = self.ptr[channel]
+        self.plots[channel].setData(self.data[channel, :i])
+        self.scroll_plots[channel].setData(self.data[channel, :i])
 
         print("update setRegion")
-        self.lr.setRegion([0, self.ptr])
+        self.max_data = max(self.max_data, i)
+        self.lr.setRegion([0, self.max_data])
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
